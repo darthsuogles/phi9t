6693f7fd52a1a4bd28d5a2dd4df2bc16aac6793a
Philip Yang <philip@quantifind.com>
2016-04-08 09:57:22 -0700

    updated installation procedure

---
+++ update_torch_distro.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+####################################
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+install_dir="${script_dir}"/torch-distro/install
+lua_ver=5.1
+####################################
+
+function _log_msg() {
+    [[ $# -ge 2 ]] || return
+    local log_type="$1"; shift    
+    >&2 echo "[${log_type}]: $@"
+}
+function log_info() { _log_msg "INFO" $@; }
+function log_warn() { _log_msg "WARNING" $@; }
+function log_error() { _log_msg "ERROR" $@; exit; }
+function quit_with() { _log_msg "QUIT" $@; exit; }
+
+
+if [ "$(basename ${script_dir})" != "torch-distro" ]; then
+    [ -d "${script_dir}/torch-distro" ] || (
+	cd ${script_dir}
+	git submodule add https://github.com/darthsuogles/torch-distro.git torch-distro --recursive
+	cd torch-distro && ./pkg_install.sh
+    )
+else
+    log_warn "(deprecated) running inside the torch-distro directory"
+    install_dir=$PWD/install
+fi
+[ -d "${install_dir}" ] || quit_with "cannot find torch install directory"
+    
+cat <<EOF
+>> Done!
+Exporting data to external environment variable file
+--------------------------------------------
+source ${script_dir}/envar_torch.sh"
+--------------------------------------------
+EOF
+
+lua_pkg_prefix="${install_dir}/share/lua/${lua_ver}"
+lua_dylib_prefix="${install_dir}/lib/lua/${lua_ver}"
+
+cat <<EOF > ${script_dir}/envar_torch.sh
+# Automatically generated 
+export LUA_PATH="${lua_pkg_prefix}/\?.lua;${lua_pkg_prefix}/\?/init.lua;./\?.lua"
+export LUA_CPATH="${lua_dylib_dir}/\?.so;./\?.d"
+export PATH="${install_dir}/bin:$PATH"
+export LD_LIBRARY_PATH="${install_dir}/lib:$LD_LIBRARY_PATH"
+export DYLD_LIBRARY_PATH="${install_dir}/lib:$DYLD_LIBRARY_PATH"
+EOF
