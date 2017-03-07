1f2837fa74c7170de56f224b4f06585b8a580f9f
Philip Yang <phi@cs.umd.edu>
2017-03-06 20:42:30 -0800

    pushed to dockerhub

--- salut.lua
+++ salut.lua
@@ -6,8 +6,7 @@ salut = load_pkg({
       NN = 'nn',
       IM = 'image', 
       TH = 'torch',
-      display = 'display', -- https://github.com/szym/display
-      'mnist', 
+      -- display = 'display', -- https://github.com/szym/display
       'io'
 }, is_repl)
 if (not is_repl) then setfenv(1, salut) end
@@ -57,29 +56,29 @@ nnet:add(NN.LogSoftMax())
 
 print(nnet)
 
--- mnist = require 'mnist'
-trainset = mnist.traindataset()
-testset = mnist.testdataset()
-print(trainset.size) -- to retrieve the size
-print(testset.size) -- to retrieve the size
+-- -- mnist = require 'mnist'
+-- trainset = mnist.traindataset()
+-- testset = mnist.testdataset()
+-- print(trainset.size) -- to retrieve the size
+-- print(testset.size) -- to retrieve the size
 
--- Some display functions
--- lena = IM.lena()
--- display.image(lena)
--- display.plot(TH.cat(TH.linspace(0, 10, 10), TH.randn(10), 2))
+-- -- Some display functions
+-- -- lena = IM.lena()
+-- -- display.image(lena)
+-- -- display.plot(TH.cat(TH.linspace(0, 10, 10), TH.randn(10), 2))
 
--- setmetatable(trainset, 
---              {__index = function(t, i) return {t.data[i], t.label[i]} end});
--- trainset.data = trainset.data:double() -- convert the data from a ByteTensor to a DoubleTensor.
--- function trainset:size() return self.data:size(1) end
+-- -- setmetatable(trainset, 
+-- --              {__index = function(t, i) return {t.data[i], t.label[i]} end});
+-- -- trainset.data = trainset.data:double() -- convert the data from a ByteTensor to a DoubleTensor.
+-- -- function trainset:size() return self.data:size(1) end
 
-criterion = NN.ClassNLLCriterion()
-nnet:add(criterion)
+-- criterion = NN.ClassNLLCriterion()
+-- nnet:add(criterion)
 
-trainer = NN.StochasticGradient(nnet, criterion)
-trainer.learningRate = 0.001
-trainer.maxIteration = 15 -- just do 5 epochs of training.
+-- trainer = NN.StochasticGradient(nnet, criterion)
+-- trainer.learningRate = 0.001
+-- trainer.maxIteration = 15 -- just do 5 epochs of training.
 
-trainer:train(trainset)
--- print(classes[testset.label[100]])
--- display.image(testset.data[100])
+-- trainer:train(trainset)
+-- -- print(classes[testset.label[100]])
+-- -- display.image(testset.data[100])
