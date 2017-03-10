1d34996d7ce8a3d830a5170da5bcc9b254738f9d
Philip Yang <phi@cs.umd.edu>
2017-03-09 20:18:52 -0800

    pytorch training

--- hw_torch.py
+++ hw_torch.py
@@ -1,25 +1,63 @@
 
 import numpy as np
 import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
-x = th.Tensor(5, 3)
-# x = torch.rand(5, 3)
-# x.size()
+def load_mnist(is_train=True):
+    return DataLoader(
+        datasets.MNIST('~/local/data', train=is_train, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=256, shuffle=True)
+    
 
-y = th.rand(5, 3)
-x + y
-y.add_(x)  # mutating y
+train_loader = load_mnist(True)
+test_loader = load_mnist(False)
 
-a = th.ones(5)
-a.numpy()  # bridge to numpy
-a.add_(1)
-b = th.from_numpy(np.ones(5))
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()  # there is a better way
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+        
 
-x = Variable(th.ones(2, 2), requires_grad=True)
-y = x + 2
-y.creator  # basic_ops.AddConstant
-z = y * y + 3
-t = z.mean()
-t.backward()
-print(x.grad)
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = F.relu(self.fc2(x))
+        return F.log_softmax(x)
+
+
+model = Net()
+optimizer = optim.SGD(model.parameters(),
+                      lr=0.01, momentum=0.5)
+        
+def train(epoch: int):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = Variable(data), Variable(target)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if 0 == batch_idx % 10:
+            print('Training Epoch: {} [{:7d}\t/{:7d} ({:2.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data[0]
+            ))
+
+train(0)
